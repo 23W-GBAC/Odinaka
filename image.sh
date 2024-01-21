9c64d4a4692d46a660d879d326103a477bb033a0
OdinakaDico <odinakaelias@gmail.com>
2024-01-21 19:29:31 +0100

    image

--- image.sh
+++ image.sh
@@ -1,17 +1,17 @@
-##!/bin/bash
+#!/bin/bash
 
-# Define your GitHub username, repository name, and personal access token
-GITHUB_USERNAME="OdinakaDico"
-REPO_NAME="Odinaka"
-ACCESS_TOKEN="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"
+Local= "/mnt/c/Users/user/Downloads/medical.jpg"
 
-# Specify the image file you want to upload
-IMAGE_FILE="path/to/your/image.png"
+Repo= "https://github.com/23W-GBAC/Odinaka/tree/main/images"
 
-# Create a new commit with the image
-curl -X PUT "https://api.github.com/repos/$GITHUB_USERNAME/$REPO_NAME/contents/images/$(basename "$IMAGE_FILE")" \
-	  -H "Authorization: token $ACCESS_TOKEN" \
-	    -d '{
-    "message": "Add image",
-        "content": "'"$(base64 -w0 "$IMAGE_FILE")"'"
-	  }'
+cp "$Local" "$Repo"
+
+cd ~/Odinaka
+
+git add .
+
+git commit -am "image"
+
+git push origin main
+
+echo "image has been uploaded successfully"
