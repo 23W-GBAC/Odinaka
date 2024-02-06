13d021c962552f1aa7f20005175c5aa1640be467
enajo <41876694+enajo@users.noreply.github.com>
2024-02-07 00:10:45 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -7,7 +7,7 @@ commit_message="Just made a push"
 git add .
 
 # Commit changes with the specified message
-git commit -am "$commit_message"
+git commit -m "$commit_message"
 
 # Push changes to the remote repository (assuming you're on the correct branch)
 git push origin main
